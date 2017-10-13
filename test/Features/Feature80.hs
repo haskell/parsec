a6194e87f5745e2ba20eab21d1b49d9e56e54d4e
Tristan Wibberley <tristan.wibberley@gmail.com>
2017-10-13 04:07:46 +0100

    Feature request #80: Add Monoid and Semigroup instances
    <> does monoidal append of parse results instead of discarding
    previous parser's results.

---
+++ test/Features/Feature80.hs
@@ -0,0 +1,51 @@
+
+module Features.Feature80
+       ( main
+       ) where
+
+import Test.HUnit hiding ( Test )
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Data.List.NonEmpty
+import Data.Semigroup
+import Data.Functor.Identity
+
+import Text.Parsec
+
+main :: Test
+main =
+  testCase "Monoid instance (#80)" $ do
+    parseString (as <> bs) "aabbb" @?= "aabbb"
+    parseString (mempty <> as) "aabbb" @?= "aa"
+    parseString (as <> mempty) "aabbb" @?= "aa"
+    parseString (sconcat $ fromList [as, mempty, bs]) "aabbb" @?= "aabbb"
+    parseString (mconcat [as, mempty, bs]) "aabbb" @?= "aabbb"
+    parseString (mempty :: ParsecT String () Identity String) "aabbb" @?= ""
+    parseString (stimes 2 str_a) "aabbb" @?= "aa"
+    parseFail   (stimes 3 str_a) "aabbb" @?= "no parse"
+    parseString ((one ch_a) <> (one ch_a) <> bs) "aabbb" @?= "aabbb"
+
+ where
+   one = fmap pure
+
+   as :: ParsecT String () Identity String
+   as = many $ char 'a'
+   bs :: ParsecT String () Identity String
+   bs = many $ char 'b'
+   ch_a :: ParsecT String () Identity Char
+   ch_a = char 'a'
+   str_a :: ParsecT String () Identity String
+   str_a = string "a"
+
+   parseString :: ParsecT String () Identity String -> String -> String
+   parseString p input =
+      case parse p "Example" input of
+        Left{} -> error "Parse failure"
+        Right str -> str
+
+   parseFail :: ParsecT String () Identity String -> String -> String
+   parseFail p input =
+      case parse p "Example" input of
+        Left{} -> "no parse"
+        Right _ -> error "Parsed but shouldn't" 
+
--- test/Main.hs
+++ test/Main.hs
@@ -2,9 +2,11 @@
 import Test.Framework
 
 import Bugs ( bugs )
+import Features ( features )
 
 main :: IO ()
 main = do
   defaultMain
     [ testGroup "Bugs" bugs
+    , testGroup "Features" features
     ]
